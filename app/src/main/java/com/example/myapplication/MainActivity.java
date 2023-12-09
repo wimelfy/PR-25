98295e56e596fe149f541da3c68fd72ea6d4ec9c
LonelyAvon <cytrikov.m@mail.ru>
2023-12-09 15:48:11 +0700

    first

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,158 @@
+package com.example.myapplication;
+
+import android.annotation.TargetApi;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SoundPool mSoundPool;
+    private AssetManager mAssetManager;
+    private int mCatSound, mChickenSound, mCowSound, mDogSound, mDuckSound, mSheepSound;
+    private int mStreamID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton cowImageButton = findViewById(R.id.image_cow);
+//        cowImageButton.setOnClickListener(onClickListener);
+
+        ImageButton chickenImageButton = findViewById(R.id.image_chicken);
+        chickenImageButton.setOnClickListener(onClickListener);
+
+        ImageButton catImageButton = findViewById(R.id.image_cat);
+        catImageButton.setOnClickListener(onClickListener);
+
+        ImageButton duckImageButton = findViewById(R.id.image_duck);
+        duckImageButton.setOnClickListener(onClickListener);
+
+        ImageButton sheepImageButton = findViewById(R.id.image_sheep);
+        sheepImageButton.setOnClickListener(onClickListener);
+
+        ImageButton dogImageButton = findViewById(R.id.image_dog);
+        dogImageButton.setOnClickListener(onClickListener);
+
+
+        cowImageButton.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                int eventAction = event.getAction();
+                if (eventAction == MotionEvent.ACTION_UP) {
+                    // Отпускаем палец
+                    if (mStreamID > 0)
+                        mSoundPool.stop(mStreamID);
+                }
+                if (eventAction == MotionEvent.ACTION_DOWN) {
+                    // Нажимаем на кнопку
+                    mStreamID = playSound(mCowSound);
+                }
+                if (event.getAction() == MotionEvent.ACTION_CANCEL) {
+                    mSoundPool.stop(mStreamID);
+                }
+                return true;
+            }
+        });
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            if (v.getId() == R.id.image_cow) {
+                playSound(mCowSound);
+            } else if (v.getId() == R.id.image_chicken) {
+                playSound(mChickenSound);
+            } else if (v.getId() == R.id.image_cat) {
+                playSound(mCatSound);
+            } else if (v.getId() == R.id.image_duck) {
+                playSound(mDuckSound);
+            } else if (v.getId() == R.id.image_sheep) {
+                playSound(mSheepSound);
+            } else if (v.getId() == R.id.image_dog) {
+                playSound(mDogSound);
+            }
+        }
+    };
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void createNewSoundPool() {
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+        mSoundPool = new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                .build();
+    }
+
+    @SuppressWarnings("deprecation")
+    private void createOldSoundPool() {
+        mSoundPool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
+    }
+
+    private int playSound(int sound) {
+        if (sound > 0) {
+            mStreamID = mSoundPool.play(sound, 1, 1, 1, 0, 1);
+        }
+        return mStreamID;
+    }
+
+    private int loadSound(String fileName) {
+        AssetFileDescriptor afd;
+        try {
+            afd = mAssetManager.openFd(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Не могу загрузить файл " + fileName,
+                    Toast.LENGTH_SHORT).show();
+            return -1;
+        }
+        return mSoundPool.load(afd, 1);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            // Для устройств до Android 5
+            createOldSoundPool();
+        } else {
+            // Для новых устройств
+            createNewSoundPool();
+        }
+
+        mAssetManager = getAssets();
+
+        // получим идентификаторы
+        mCatSound = loadSound("cat.ogg");
+        mChickenSound = loadSound("chicken.ogg");
+        mCowSound = loadSound("cow.ogg");
+        mDogSound = loadSound("dog.ogg");
+        mDuckSound = loadSound("duck.ogg");
+        mSheepSound = loadSound("sheep.ogg");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSoundPool.release();
+        mSoundPool = null;
+    }
+}
